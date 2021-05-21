a04daf04cdbaa5c24a632b4b93ccabca50522cff
Raghunathji <partabrajp@gmail.com>
2021-05-22 00:30:46 +0500

    upload to git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      jay sia raam
+      <br />
+      jay raghubeer gosai
+      <br />
+      namah parvati pataye
+      <br />
+      jay sia raam from netlify
     </div>
   );
 }
